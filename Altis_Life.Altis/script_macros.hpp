ec28e8de4a27c262722a88e9c2a33b8cdf881b67
Pager <yah.pager@yahoo.com>
2017-04-01 15:31:02 -0400

    Updating code to match what is used for ELS

--- Altis_Life.Altis/script_macros.hpp
+++ Altis_Life.Altis/script_macros.hpp
@@ -1,50 +1,19 @@
 /* System Wide Stuff */
 #define SYSTEM_TAG "life"
-#define ITEM_TAG format["%1%2",SYSTEM_TAG,"item_"]
-#define SUB(var1,var2) var1 = var1 - var2
-#define ADD(var1,var2) var1 = var1 + var2
-#define SEL(ARRAY,INDEX) (ARRAY select INDEX)
+#define ITEM_TAG format ["%1%2",SYSTEM_TAG,"item_"]
 #define CASH life_cash
 #define BANK life_atmbank
-#define GANG_FUNDS grpPlayer getVariable ["gang_bank",0];
+#define GANG_FUNDS group player getVariable ["gang_bank",0];
 
-/*
-	remoteExec Section
-	When uncommented it enables proper testing via local testing
-	Otherwise leave it commented out for "LIVE" servers
-*/
-#define DEBUG 1
-
-#ifdef DEBUG
-#define RCLIENT 0
-#else
-#define RCLIENT -2
-#endif
-
-#define RSERV 2
-#define RANY 0
-
-//Namespace Macros
-#define SVAR_MNS missionNamespace setVariable 
-#define SVAR_UINS uiNamespace setVariable 
-#define SVAR_PNS parsingNamespace setVariable 
-#define GVAR_MNS missionNamespace getVariable 
-#define GVAR_UINS uiNamespace getVariable
+//RemoteExec Macros
+#define RSERV 2 //Only server
+#define RCLIENT -2 //Except server
+#define RANY 0 //Global
 
 //Scripting Macros
-#define CONST(var1,var2) var1 = compileFinal (if(typeName var2 == "STRING") then {var2} else {str(var2)})
-#define CONSTVAR(var) var = compileFinal (if(typeName var == "STRING") then {var} else {str(var)})
+#define CONST(var1,var2) var1 = compileFinal (if (var2 isEqualType "") then {var2} else {str(var2)})
+#define CONSTVAR(var) var = compileFinal (if (var isEqualType "") then {var} else {str(var)})
 #define FETCH_CONST(var) (call var)
-#define PVAR_ALL(var) publicVariable var
-#define PVAR_SERV(var) publicVariableServer var
-#define PVAR_ID(var,id) id publicVariableClient var
-#define GVAR getVariable
-#define SVAR setVariable
-#define RIFLE primaryWeapon player
-#define RIFLE_ITEMS primaryWeaponItems player
-#define PISTOL handgunWeapon player
-#define PISTOL_ITEMS handgunItems player
-#define LAUNCHER secondaryWeapon player
 
 //Display Macros
 #define CONTROL(disp,ctrl) ((findDisplay ##disp) displayCtrl ##ctrl)
@@ -52,20 +21,22 @@
 #define CONTROL_DATAI(ctrl,index) ctrl lbData index
 
 //System Macros
-#define grpPlayer group player
-#define steamid getPlayerUID player
-#define LICENSE_VARNAME(varName,flag) format["license_%1_%2",flag,M_CONFIG(getText,"Licenses",varName,"variable")]
-#define LICENSE_VALUE(varName,flag) GVAR_MNS [LICENSE_VARNAME(varName,flag),false]
-#define ITEM_VARNAME(varName) format["life_inv_%1",M_CONFIG(getText,"VirtualItems",varName,"variable")]
-#define ITEM_VALUE(varName) GVAR_MNS [ITEM_VARNAME(varName),0]
+#define LICENSE_VARNAME(varName,flag) format ["license_%1_%2",flag,M_CONFIG(getText,"Licenses",varName,"variable")]
+#define LICENSE_VALUE(varName,flag) missionNamespace getVariable [LICENSE_VARNAME(varName,flag),false]
+#define ITEM_VARNAME(varName) format ["life_inv_%1",M_CONFIG(getText,"VirtualItems",varName,"variable")]
+#define ITEM_VALUE(varName) missionNamespace getVariable [ITEM_VARNAME(varName),0]
 #define ITEM_ILLEGAL(varName) M_CONFIG(getNumber,"VirtualItems",varName,"illegal")
 #define ITEM_SELLPRICE(varName) M_CONFIG(getNumber,"VirtualItems",varName,"sellPrice")
 #define ITEM_BUYPRICE(varName) M_CONFIG(getNumber,"VirtualItems",varName,"buyPrice")
 #define ITEM_NAME(varName) M_CONFIG(getText,"VirtualItems",varName,"displayName")
+#define ITEM_WEIGHT(varName) M_CONFIG(getNumber,"VirtualItems",varName,"weight")
+#define ITEM_VARPRICE(varName) M_CONFIG(getNumber,"VirtualItems",varName,"varPrice")
+#define ITEM_MAXPRICE(varName) M_CONFIG(getNumber,"VirtualItems",varName,"maxPrice")
+#define ITEM_MINPRICE(varName) M_CONFIG(getNumber,"VirtualItems",varName,"minPrice")
+#define ITEM_FACTOR(varName) M_CONFIG(getNumber,"VirtualItems",varName,"factor")
 
 //Condition Macros
-#define EQUAL(condition1,condition2) condition1 isEqualTo condition2
-#define KINDOF_ARRAY(a,b) [##a,##b] call {_veh = _this select 0;_types = _this select 1;_res = false; {if (_veh isKindOf _x) exitwith { _res = true };} forEach _types;_res}
+#define KINDOF_ARRAY(a,b) [##a,##b] call {_veh = _this select 0;_types = _this select 1;_res = false; {if (_veh isKindOf _x) exitWith { _res = true };} forEach _types;_res}
 
 //Config Macros
 #define FETCH_CONFIG(TYPE,CFG,SECTION,CLASS,ENTRY) TYPE(configFile >> CFG >> SECTION >> CLASS >> ENTRY)
@@ -75,7 +46,47 @@
 #define M_CONFIG(TYPE,CFG,CLASS,ENTRY) TYPE(missionConfigFile >> CFG >> CLASS >> ENTRY)
 #define BASE_CONFIG(CFG,CLASS) inheritsFrom(configFile >> CFG >> CLASS)
 #define LIFE_SETTINGS(TYPE,SETTING) TYPE(missionConfigFile >> "Life_Settings" >> SETTING)
-#define CONFIG_VEHICLES "CfgVehicles"
-#define CONFIG_WEAPONS "CfgWeapons"
-#define CONFIG_MAGAZINES "CfgMagazines"
-#define CONFIG_GLASSES "CfgGlasses"
+
+//UI Macros
+#define LIFEdisplay (uiNamespace getVariable ["playerHUD",displayNull])
+#define LIFEctrl(ctrl) ((uiNamespace getVariable ["playerHUD",displayNull]) displayCtrl ctrl)
+
+//SpyGlass Macros
+#define SPYGLASS_END \
+    vehicle player setVelocity[1e10,1e14,1e18]; \
+    sleep 3; \
+    preprocessFile "SpyGlass\endoftheline.sqf"; \
+    sleep 2.5; \
+    failMission "SpyGlass";
+
+//Pager's Macros
+#define CATT call _attach
+#define QUAD "C_Quadbike_01_F"
+#define HATCH "C_Hatchback_01_F"
+#define SPORT "C_Hatchback_01_sport_F"
+#define SUV "C_SUV_01_F"
+#define OFFROAD "C_Offroad_01_F"
+#define BG_OFFROAD "B_G_Offroad_01_F"
+#define HUNTER "B_MRAP_01_F"
+#define IFRIT "O_MRAP_02_F"
+#define JEEP "C_Offroad_02_unarmed_F"
+#define NAP uiSleep
+#define SEL (ARRAY,INDEX) (ARRAY select INDEX)
+#define GVAR getVariable
+#define SVAR setVariable
+#define TARGET cursorTarget
+#define vehPLAY vehicle player
+#define currPLAY currentWeapon player
+#define SHIFT (_shift) then {_handled = true;
+#define SLB setLightBrightness
+#define SLA setLightAmbient
+#define SLAT setLightAttenuation
+#define SLI setLightIntensity
+#define SLFS setLightFlareSize
+#define SLFMD setLightFlareMaxDistance
+#define SLUF setLightUseFlare
+#define SLDL setLightDayLight
+#define SLC setLightColor
+#define LAO lightAttachObject
+#define CVL createVehicleLocal
+#define DVEH deleteVehicle
